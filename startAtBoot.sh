991b3d5ee6dd076dfb94dd808a2f320914bd6bc0
EC2 Default User <ec2-user@ip-10-10-46-45.ap-southeast-1.compute.internal>
2020-06-13 10:12:07 +0000

    Fixed configurations

--- startAtBoot.sh
+++ startAtBoot.sh
@@ -1,4 +1,5 @@
 cp nginx-setup/nginx /etc/init.d/.
+chmod +x /etc/init.d/nginx
 sudo npm install pm2 -g
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemv -u ec2-user --hp /home/ec2-user
 pm2 start index.js --name="cicd-server"
